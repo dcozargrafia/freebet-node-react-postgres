0e71d3d847c5f5917ead2dd009defe9cb7bcfda4
Daniel Cózar <dcozargrafia@gmail.com>
2025-01-04 21:02:21 +0100

    Añadida ruta para mostrar las casas de apuestas.

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER, FRONTEND_URL, PORT
 const app = express();
 
 const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://freebet_db_bkri_user:wmVPq6fyclMBzQpzNGjPUGonzZQiJYvJ@dpg-cts9ult2ng1s73bvl3lg-a.frankfurt-postgres.render.com/freebet_db_bkri',
+    connectionString: process.env.DATABASE_URL || 'postgres://freebet_db_bkri_user:wmVPq6fyclMBzQpzNGjPUGonzZQiJYvJ@dpg-cts9ult2ng1s73bvl3lg-a.frankfurt-postgres.render.com/freebet_db_bkri',
     ssl: {
         rejectUnauthorized: false
     },
@@ -33,6 +33,15 @@ app.get('/ping', async (req, res) => {
         }
     )
 });
+
+app.get('/casas-apuestas', async (req, res) => {
+    const result = await pool.query('SELECT * FROM casas_de_apuestas');
+    res.send(
+        {
+            casas_apuestas: result.rows
+        }
+    );
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
