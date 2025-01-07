955aca153d6805717361856f37ea39480f30e33b
Daniel Cózar <dcozargrafia@gmail.com>
2025-01-07 12:45:29 +0100

    conexion front-back

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@
 import express from 'express';
 import cors from 'cors';
 
+import { pool } from './database.js';
+
 import { FRONTEND_URL, PORT } from './config.js';
 
 import bookmakersRoutes from './routes/bookmakers.routes.js';
 import transactionsRoutes from './routes/transactions.routes.js';
 import casinosRoutes from './routes/casinos.routes.js';
 import betsRoutes from './routes/bets.routes.js';
+
 
 const app = express();
 
@@ -23,6 +26,13 @@ app.use('/bookmakers', bookmakersRoutes);
 app.use('/transactions', transactionsRoutes);
 app.use('/casinos', casinosRoutes);
 app.use('/bets', betsRoutes);
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()');
+    res.send({
+        pong: result.rows[0].now
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
